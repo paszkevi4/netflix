8568856562d427eda210253d0efad46f674f6c6d
paszkevi4 <paszkevi4@gmail.com>
2020-09-27 08:28:21 +0300

    Updates

---
+++ precache-manifest.dd8a5885e45dfed5aa162f7eac7221e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bedcae8afbcb80462ac87d3de1d6301f",
+    "url": "/netflix/index.html"
+  },
+  {
+    "revision": "e9b3e95d23e063b08c3c",
+    "url": "/netflix/static/css/main.145316d5.chunk.css"
+  },
+  {
+    "revision": "692e8790ac1f9499b9ea",
+    "url": "/netflix/static/js/2.58b24286.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/netflix/static/js/2.58b24286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8401a518ad981596529c",
+    "url": "/netflix/static/js/3.b1aedaab.chunk.js"
+  },
+  {
+    "revision": "e9b3e95d23e063b08c3c",
+    "url": "/netflix/static/js/main.09c6620b.chunk.js"
+  },
+  {
+    "revision": "80d8ed74108b10da979e",
+    "url": "/netflix/static/js/runtime-main.bc987d32.js"
+  },
+  {
+    "revision": "f4c0b89993fffdb887a6021e8433bddf",
+    "url": "/netflix/static/media/lucifer.f4c0b899.png"
+  },
+  {
+    "revision": "d0ecf7b6f3f8966edc319d0b94942461",
+    "url": "/netflix/static/media/profile.d0ecf7b6.jpg"
+  }
+]);
